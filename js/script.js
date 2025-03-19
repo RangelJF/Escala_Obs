df452e01e65755754770eea7562e410d800a8c75
RANGEL FERREIRA <ferreirarangel07@gmail.com>
2025-03-18 23:06:40 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,14 @@
-const selectedDays = [2, 5, 9, 12, 16, 19, 23, 26, 30];
+const selectedDays = [2, 5, 9, 12, 16, 19, 23, 26,30];
 const daysContainer = document.getElementById('daysContainer');
 const monthSelect = document.getElementById('month');
 const form = document.getElementById('availabilityForm');
 
-// Função de obter o nome do dia da semana
 function getDayName(day, month) {
     const daysOfWeek = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
     const date = new Date(2025, month - 1, day);
     return daysOfWeek[date.getDay()];
 }
 
-// Event listener para o select de mês
 monthSelect.addEventListener('change', () => {
     const month = parseInt(monthSelect.value);
     daysContainer.innerHTML = '';
@@ -33,11 +31,11 @@ monthSelect.addEventListener('change', () => {
             const dayName = getDayName(day, month);
             const checkbox = document.createElement('input');
             checkbox.type = 'checkbox';
-            checkbox.id = `day${day}`;
+            checkbox.id = day;
             checkbox.name = "selectedDays[]";
             checkbox.value = day;
             const label = document.createElement('label');
-            label.htmlFor = `day${day}`;
+            label.htmlFor = day;
             label.textContent = `Dia ${day} (${dayName})`;
             label.style.color = "#fff";
             const div = document.createElement('div');
@@ -46,86 +44,4 @@ monthSelect.addEventListener('change', () => {
             daysContainer.appendChild(div);
         });
 
-        const unavailableCheckboxElement = document.getElementById('unavailable');
-        const cryingMessage = document.getElementById('cryingMessage');
-        const dayCheckboxes = document.querySelectorAll('#daysContainer input[type="checkbox"]:not(#unavailable)');
-
-        unavailableCheckboxElement.addEventListener('change', () => {
-            if (unavailableCheckboxElement.checked) {
-                cryingMessage.style.display = 'block';
-                dayCheckboxes.forEach(checkbox => checkbox.disabled = true);
-            } else {
-                cryingMessage.style.display = 'none';
-                dayCheckboxes.forEach(checkbox => checkbox.disabled = false);
-            }
-        });
-
-        dayCheckboxes.forEach(checkbox => {
-            checkbox.addEventListener('change', () => {
-                if (checkbox.checked) {
-                    unavailableCheckboxElement.disabled = true;
-                } else {
-                    unavailableCheckboxElement.disabled = false;
-                }
-            });
-        });
-
-        document.getElementById('closeButton').addEventListener('click', () => {
-            cryingMessage.style.display = 'none';
-        });
-    }
-});
-
-// Função de envio do formulário
-form.addEventListener('submit', async function(event) {
-    event.preventDefault();
-    const submitButton = document.querySelector('button[type="submit"]');
-    submitButton.disabled = true;
-    document.getElementById('loadingMessage').style.display = 'block';
-
-    const formData = new FormData(this);
-    const month = monthSelect.options[monthSelect.selectedIndex].text;
-    const name = formData.get('name');
-    const selectedDays = formData.getAll('selectedDays[]');
-
-    if (selectedDays.length === 0) {
-        alert('Você precisa selecionar pelo menos um dia ou marcar a opção de "Indisponível".');
-        submitButton.disabled = false;
-        document.getElementById('loadingMessage').style.display = 'none';
-        return;
-    }
-
-    const params = new URLSearchParams();
-    params.append('month', month);
-    params.append('name', name);
-    params.append('selectedDays', selectedDays.join(','));
-
-    try {
-        const response = await fetch('https://script.google.com/macros/s/AKfycbwukn-WcfDjDJCX2OR2hYGxV38pS2G67_9mkARd3OzC_8O9Df83UzaMgUHr2-ZuY1FP/exec', {
-            method: 'POST',
-            body: params
-        });
-
-        if (!response.ok) {
-            throw new Error(`Erro na requisição: ${response.status} ${response.statusText}`);
-        }
-
-        const responseData = await response.text();
-        console.log(responseData);
-
-        if (responseData.includes('erro')) {
-            throw new Error('O servidor retornou um erro ao processar os dados.');
-        }
-
-        this.style.display = 'none';
-        document.getElementById('successMessage').style.display = 'block';
-        document.getElementById('alertBox').style.display = 'block';
-    } catch (error) {
-        console.error('Erro ao enviar os dados:', error);
-        document.getElementById('alertBox').style.display = 'block';
-        alert(`Erro ao enviar os dados: ${error.message}`);
-    } finally {
-        document.getElementById('loadingMessage').style.display = 'none';
-        submitButton.disabled = false;
-    }
-});
+        const unavailableCheckboxElement = document.getElementById
